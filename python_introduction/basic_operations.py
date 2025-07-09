ff4a2e02fe3eaaaa12ed0fbc8749414cc9894c34
Kingsley Iwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-07-09 23:44:02 +0100

    Update

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,23 @@
+# Declaring Variable
+number1 = 10
+number2 = 5
+
+#Addition of two number
+
+sum = number1 + number2
+
+# Substraction of two number
+
+diff = number2 - number1
+
+# Multiplication of two number
+
+product = number1 * number2
+
+# display the result
+
+print("Addition of ", number1, "and", number2, "is", sum )
+
+print("Subtraction of ", number2, "and", number1, "is", diff )
+
+print("Multiplication of ", number1, "and", number2, "is", product)
