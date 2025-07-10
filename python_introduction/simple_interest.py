f91163b6d62c3ad2824316253e2d88bffc25c4a8
Kingsley Iwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-07-10 21:40:28 +0100

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+# This a simple Python script that calculates the simple interest earned on an investment over a period of time.
+#The formula for simple interest is (I = P * R * T).
+
+# Declared 3 variable
+principal = int(1000)
+rate = float(0.05)
+time = 3
+
+# calculate the simple interest
+interest = principal * rate * time
+
+# Display the result.
+print(f"The simple interest is: ${interest}")
