ac98e1478d102d2dd34ca4dd20ea4d5cfe470013
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-08-03 15:22:54 +0100

    Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+# Objective: Understand the fundamentals of OOP in Python by implementing a BankAccount class that encapsulates banking operations. 
+# Use command line arguments to interact with instances of this class.
+
+class BankAccount:
+    def __init__(self, initial_balance):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
